94615db3dbcca2755c211094bdfb701a2d6a9275
João Alencar <89602176+Joaoalen98@users.noreply.github.com>
2024-09-03 22:26:50 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nmuqissimzuovfnelusq.supabase.co/rest/v1',
+    baseURL: env.BASE_URL,
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tdXFpc3NpbXp1b3ZmbmVsdXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MDkwNDUsImV4cCI6MjA0MDk4NTA0NX0.jaInubdmUossjq4ZULF1JUKm_kA3J_nLx2JEggibhso",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tdXFpc3NpbXp1b3ZmbmVsdXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MDkwNDUsImV4cCI6MjA0MDk4NTA0NX0.jaInubdmUossjq4ZULF1JUKm_kA3J_nLx2JEggibhso"
+        apikey: env.API_KEY,
+        authorization: env.BEARER_TOKEN
     }
 })
